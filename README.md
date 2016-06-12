5b256935ccda831cbc1934a9cf39071afba764a8
Chen-You <harry60379@yahoo.com.tw>
2016-06-12 20:30:49 +0800

    finish 1st

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Assignment8
+
+先處理client的部分
+
+	這次主要步驟是先處理一開始輸入ip的部分(並且多新增一個.xml檔案)，再來是reuse在lab10所用的部分，以及
+新增thread來完成。
+
+再來是處理server的部分
+
+	在server的部分，主要是reuse助教提供的thread的部分，並且在多新增JFrame以及Jtextarea來實作完成。
---
+++ server/src/main/java/com/example/Server.java
@@ -0,0 +1,83 @@
+package com.example;
+
+import java.awt.BorderLayout;
+import java.awt.SystemColor;
+import java.io.InputStream;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import javax.swing.JFrame;
+import javax.swing.JTextArea;
+
+/**
+ * Created by Gary on 16/5/28.
+ */
+public class Server implements Runnable{
+    private Thread thread;
+    private ServerSocket servSock;
+
+    private JFrame frame;
+    private JTextArea textArea;
+
+
+    public Server(){
+
+        frame = new JFrame();
+        frame.setSize(500, 300);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        textArea = new JTextArea();
+        textArea.setBackground(SystemColor.textHighlight);
+
+        frame.getContentPane().add(BorderLayout.CENTER, textArea);
+        frame.setVisible(true);
+
+        try {
+            // Detect server ip
+            InetAddress IP = InetAddress.getLocalHost();
+            textArea.setText("IP of my system is :"+IP.getHostAddress() + "\n" + "Waitting to connect......");
+
+            // Create server socket
+            servSock = new ServerSocket(2000);
+
+            // Create socket thread
+            thread = new Thread(this);
+            thread.start();
+        } catch (java.io.IOException e) {
+            System.out.println("Socket啟動有問題 !");
+            System.out.println("IOException :" + e.toString());
+        } finally{
+
+        }
+    }
+
+    @Override
+    public void run(){
+        // Running for waitting multiple client
+        while(true){
+            try{
+                // After client connected, create client socket connect with client
+                //System.out.println("I am here~~");
+                Socket clntSock = servSock.accept();
+                InputStream in = clntSock.getInputStream();
+
+                System.out.println("Connected");
+                textArea.setText("Connected!!");
+
+                // Transfer data
+                byte[] b = new byte[1024];
+                int length;
+
+                length = in.read(b);
+                String s = new String(b);
+                System.out.println("[Server Said]" + s);
+                textArea.setText(s);
+
+            }
+            catch(Exception e){
+                //System.out.println("Error: "+e.getMessage());
+            }
+        }
+    }
+}
